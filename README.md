b6d0df600c7d1b45276d68a2d3f40ed74acf9663
Kerem Kayabay <kerem.kayabay@gmail.com>
2020-04-19 15:35:11 +0300

    show alerts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exploding-tabs
-Set up a countdown timer for addictive sites.
+Stop addictive sites from stealing your focus by setting up a countdown timer.
+
+The tab is closed when the countdown reaches 0. Default countdown is 3 minutes but you can adjust that for each site. To stop the countdown, click the stopwatch icon in the address bar.
--- content_scripts/countdown.js
+++ content_scripts/countdown.js
@@ -12,6 +12,10 @@
     var countdown = null;
     var countdownInProgress = false;
     var timeLeft = 180;
+    var timePassed = 0;
+    var countdownBodyElement = document.createElement("div");
+    var countdownBodyElementVisible = false;
+    var userClosedCountdownBodyElement = false;
 
     function onError(error) {
         console.log(error);
@@ -22,6 +26,9 @@
       if( countdownInProgress ){
         return;
       }
+
+      insertCountdownBodyElement();
+
       countdownInProgress = true;
       timeLeft = timer;
       countdown = setInterval(tick, 1000);
@@ -29,18 +36,99 @@
     
     function tick(){
       timeLeft--;
+      timePassed++;
+
+      document.title = "Time left: " + timeLeft;
+      countdownBodyElement.innerText = "This tab will destroy itself in " + timeLeft + " seconds."; 
+
+      if(timeLeft == 30){
+        userClosedCountdownBodyElement = false;
+      }
 
       if( timeLeft < 1 ){
         browser.runtime.sendMessage({greeting: "Greeting from the content script"});
         clearInterval(countdown);
       }
-      document.title = "Time left: " + timeLeft;
+      else if( timeLeft < 30  ){
+        openCountdownBodyElement();
+      }
+      else if( timePassed > 9 ){
+        closeCountdownBodyElement();
+      }
+      else if( timePassed == 1 ){
+        openCountdownBodyElement();
+      }
     }
 
     function stopCountdown() {
+      closeCountdownBodyElement();
       clearInterval(countdown);
     }
-  
+    
+    function insertCountdownBodyElement(){
+      countdownBodyElement.style.display = 'none';
+      countdownBodyElement.style.position = "fixed";
+      countdownBodyElement.style.top = "-30px";
+      countdownBodyElement.style.left = "25%";
+      countdownBodyElement.style.width = "50%";
+      countdownBodyElement.style.paddingTop = "30px";
+      countdownBodyElement.style.paddingLeft = "5px";
+      countdownBodyElement.style.backgroundColor = "white";
+      countdownBodyElement.style.fontFamily = "Helvetica, Arial, sans-serif";
+      countdownBodyElement.style.fontSize = "15px";
+      //countdownBodyElement.style.borderBottom = "2px solid #000";
+      countdownBodyElement.style.boxShadow = "5px 5px 0px grey";
+      countdownBodyElement.style.cursor = "pointer";
+      countdownBodyElement.style.zIndex = "999999999";
+      countdownBodyElement.addEventListener("click", clickOnCountdownBodyElement);
+      document.getElementsByTagName('body')[0].prepend( countdownBodyElement );
+    }
+
+    function openCountdownBodyElement(){
+      if( countdownBodyElementVisible || userClosedCountdownBodyElement ){
+        return;
+      }
+      
+      countdownBodyElement.style.display = 'block';
+      countdownBodyElementVisible = true;
+
+      var pos = -30;
+      var id = setInterval(animate, 5);
+      function animate() {
+        if (pos == 0) {
+          clearInterval(id);
+        } else {
+          pos++; 
+          countdownBodyElement.style.top = pos + "px";
+        }
+      }
+    }
+
+    function clickOnCountdownBodyElement(){
+      userClosedCountdownBodyElement = true;
+      closeCountdownBodyElement();
+    }
+
+    function closeCountdownBodyElement(){
+      if( !countdownBodyElementVisible ){
+        return;
+      }
+
+      countdownBodyElementVisible = false;
+
+      var pos = 0;
+      var id = setInterval(animate, 5);
+      function animate() {
+        if (pos == -30) {
+          countdownBodyElement.style.display = 'none';
+          clearInterval(id);
+        } else {
+          pos--; 
+          countdownBodyElement.style.top = pos + "px";
+        }
+      }
+    }
+
     /**
      * Listen for messages from the background script.
     */
